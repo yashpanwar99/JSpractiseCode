2c886c59143d00af86f9559338f9c141f8b240bb
Yash1003 <yashpanwar99@gmail.com>
2021-12-08 13:34:44 +0530

    8th dec (2nd commit)

---
+++ js/10.wndowObjcts.js
@@ -0,0 +1,19 @@
+console.log("Window Objects");
+
+// window is a global object in client side js
+
+let  a = window;
+// window.alert("Hello Peter"); // gives a alert to the user
+    //   a = prompt("I'm gonna beat you");  // prompt will save the input in a variable.
+    //   a = confirm("Are you sure , you want to reload"); // It will give us a boolean value.
+    //   a = window.innerHeight;
+    //   a = window.innerWidth;
+    //   a = window.scrollX;   // gives the scroll perct. in X
+    //   a= scrollY;  // gives the scroll perct. in Y            
+    //   a = location;
+    //   a= location.href;
+    //   a = location.toString();
+         a = window.history;    
+// We don't need to write window as its a global object
+console.log(a);
+
