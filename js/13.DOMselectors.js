dd548e786faa341b0f38504d8d264d86b510cccc
Yash1003 <yashpanwar99@gmail.com>
2021-12-11 12:23:22 +0530

    11th Dec 1st commit

--- js/13.DOMselectors.js
+++ js/13.DOMselectors.js
@@ -5,6 +5,8 @@ console.log("We'll learn about HTML Selectors");
        2). Multi Element Selector
 */
 
+//  Single Element Selectors
+
 let m = document.getElementById('myFirst');
 // m = m.className;
 // m = m.childNodes;
@@ -12,6 +14,25 @@ let m = document.getElementById('myFirst');
 // m=m.parentElement;
 // m=m.style.color='red';    // We can also style the elements using DOM Selectors
 // m.innerText='Yash Panwar';
-m.innerHTML='<b>Yash Panwar</b>';
-console.log(m);
+// m.innerHTML='<b>Yash Panwar</b>';
+// console.log(m);
 // console.log(m.innerText);
+
+
+//  QUERY SELECTORs
+
+let n = document.querySelector('#myFirst');
+n = document .querySelector('.child');
+n=document.querySelector('div');
+n=document.querySelector('b');
+n=document.querySelector('h1');
+n.style.color=('green');
+console.log(n);
+
+
+// Multi Element Selector
+
+let elems = document.getElementsByClassName('child');
+    elems = document.getElementsByTagName('div');
+console.log(elems);
+
---
+++ js/14.traversingDOM.js
@@ -0,0 +1,46 @@
+console.log("We'll learn DOM Traversal");
+
+let cont = document.querySelector('.no');
+cont = document.querySelector('.container');
+// console.log(cont);
+// console.log(cont.childNodes); // It gives text , comments etc. too so we use children as it gives only elements.
+// console.log(cont.children);
+// let nodeName = cont.childNodes[1].nodeName;
+// console.log(nodeName);
+
+/* Node Types - 
+
+    1. Element
+    2. Attribute
+    3. Text Node
+    8. Comment
+    9. Document
+   10. DocType
+
+*/
+// let nodeType = cont.childNodes[1].nodeType;
+// console.log(nodeType);
+
+
+// Accessing children's children
+
+let con = document.querySelector('div.container');
+// console.log(con.children[0].children);
+// console.log(con.children[0].children[0].children);
+
+// console.log(con.childNodes);
+
+// console.log(con.firstChild);
+// console.log(con.firstElementChild);
+
+// console.log(con.lastChild);
+// console.log(con.lastElementChild);
+
+// console.log(con.children);
+// console.log(con.childElementCount);
+
+console.log(con.firstElementChild);
+console.log(con.firstElementChild.parentNode);
+console.log(con.firstElementChild.nextSibling);
+console.log(con.firstElementChild.nextElementSibling);
+
