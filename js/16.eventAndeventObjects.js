6b9cf5b82caaf0c3329f4b76494a197f0c98ac5e
Yash1003 <yashpanwar99@gmail.com>
2021-12-13 13:28:02 +0530

    13th Dec (1st)

---
+++ js/16.eventAndeventObjects.js
@@ -0,0 +1,18 @@
+console.log('We will learn about event and event objects');
+
+document.getElementById('heading').addEventListener('click' , function(e){
+    console.log('hello peter!!!!!');
+    // console.log(e);
+    // let a = e.target.className;
+    // a = e.target.classList;
+    // a = Array.from(e.target.classList);
+    // let a = e.target.id;
+    // a = e.offsetX;
+    // a = e.offsetY;
+    // a = e.clientX;
+    // a= e.clientY; 
+    console.log(a)
+// location.href = '//google.com';
+
+
+});
---
+++ js/17.moreEvents.js
@@ -0,0 +1,46 @@
+console.log("We'll learn more about events");
+
+// let a = document.getElementById('btn')
+// a.addEventListener('click' , func1);
+// a.addEventListener('dblclick' , func2);
+// a.addEventListener('mousedown' , func3);
+
+/* e is called event object */
+
+// function func1(e){
+//     console.log('it is a click');
+//     console.log(e);
+//     e.preventDefault();
+// }
+
+
+// function func2(e){
+//     console.log('It is a double click');
+//     console.log(e);
+//     e.preventDefault();
+// }
+
+
+// function func3(e){
+//     console.log('it is a mouse down');
+//     console.log(e);
+//     e.preventDefault();
+// }
+
+
+// document.querySelector('.no').addEventListener('mouseenter',funm );
+// function funm(e){
+//     console.log('You enter the div');
+// }
+
+// document.querySelector('.no').addEventListener('mouseleave',function(e){
+//     console.log('You leave the div');
+// });
+
+// document.querySelector('.container').addEventListener('mousemove',function(e){
+        
+//         console.log(e.offsetX , e.offsetY);
+//         document.body.style.backgroundColor=`rgb(${e.offsetX},${e.offsetY},${99})`;
+//         console.log('You move in the div');
+//     });
+
